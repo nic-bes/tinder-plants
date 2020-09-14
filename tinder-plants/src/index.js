9753f723d794657aed00789f3c23d6cdbfecc333
Antonia Symeonidou <antonia.symeonidou@outlook.com>
2020-09-14 16:26:22 +0100

    basic form

--- tinder-plants/src/index.js
+++ tinder-plants/src/index.js
@@ -1,13 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
-import Login from "./components/Login";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Router exact path="/login">
-      <Login />
-    </Router>
-  </React.StrictMode>,
-  document.getElementById("root")
+  <App />,
+  document.getElementById('root')
 );
